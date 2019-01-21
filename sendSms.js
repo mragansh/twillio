752a990a1f5796ac0387c562792a88ef3354bebb
Mragansh Tomar <mraganshtomar@rocketmail.com>
2019-01-21 15:01:29 +0530

    Update sendSms.js

--- sendSms.js
+++ sendSms.js
@@ -1,5 +1,4 @@
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Your Account Sid and Auth Token from twilio.com/console
+
 const accountSid = 'AC53295456c7680671587990cfa89adb37';
 const authToken = 'd330b74f1ebf846dbd0b601f9e0107b2';
 const client = require('twilio')(accountSid, authToken);
